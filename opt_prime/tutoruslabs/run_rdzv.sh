f190734901f43e51defc545d353d45b0c171a392
Minyoung Kyoung <mykyoung@tutoruslabs.com>
2025-11-10 07:46:22 +0000

    debug4

--- opt_prime/tutoruslabs/run_rdzv.sh
+++ opt_prime/tutoruslabs/run_rdzv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export NCCL_DEBUG=ERROR
-#export TORCH_DIST_INIT_BARRIER=1
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_BLOCKING_WAIT=1
+export TORCH_DIST_INIT_BARRIER=1
 
 ############################################
 # User params
